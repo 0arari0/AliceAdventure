bf22c18b17ba944dbea41667c810f79bfe11b062
LostMoa <life4458@naver.com>
2022-08-13 03:45:41 +0900

    업데이트

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public float moveSpeed;
+    public ItemType_ itemType;
+    public enum ItemType_
+    { MoveSpeedUp, DamageUp, Shield }
+
+    void Update()
+    {
+        gameObject.transform.Translate(new Vector2(0, -1 * moveSpeed * Time.deltaTime));
+    }
+}
---
+++ Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuButton : MonoBehaviour
+{
+    public GameObject howPlayWindow, storyWindow, optionWindow; // 메인메뉴 버튼별 윈도우
+    public GameObject howPlayGrayWindow, storyGrayWindow, optionGrayWindow;
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    public void SelectGameStartButton()
+    {
+        // 메인 메뉴에서 게임 시작 버튼 눌렀을 때 실행
+        SoundManager.instance.PlaySfx(SoundManager.SFX_Name_.ButtonClick);
+        try
+        {
+            SceneManager.LoadScene("BattleRound1");
+        }
+        catch { }
+    }
+
+    public void SelectHowPlayButton()
+    {
+        // 메인 메뉴에서 조작 방법 버튼 눌렀을 때 실행
+        // 조작 방법 창에서 닫기 버튼 눌렀을 때 실행
+        SoundManager.instance.PlaySfx(SoundManager.SFX_Name_.ButtonClick);
+        howPlayGrayWindow.SetActive(!howPlayGrayWindow.activeSelf);
+        howPlayWindow.SetActive(!howPlayWindow.activeSelf);
+    }
+
+    public void SelectShowStoryButton()
+    {
+        // 메인 메뉴에서 스토리 열람 버튼 눌렀을 때 실행
+        // 스토리 열람 창에서 닫기 버튼 눌렀을 때 실행
+        SoundManager.instance.PlaySfx(SoundManager.SFX_Name_.ButtonClick);
+        storyGrayWindow.SetActive(!storyGrayWindow.activeSelf);
+        storyWindow.SetActive(!storyWindow.activeSelf);
+    }
+
+    public void SelectOptionButtonOpen()
+    {
+        // 메인 메뉴에서 옵션 버튼 눌렀을 때 실행
+        SoundManager.instance.PlaySfx(SoundManager.SFX_Name_.ButtonClick);
+        optionGrayWindow.SetActive(true);
+        optionWindow.SetActive(true);
+    }
+
+    public void SelectGameExitButton()
+    {
+        // 메인 메뉴에서 게임 종료 버튼 눌렀을 때 실행
+        SoundManager.instance.PlaySfx(SoundManager.SFX_Name_.ButtonClick);
+        Application.Quit();
+    }
+}
