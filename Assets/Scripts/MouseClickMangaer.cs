355918cafb2d1c1cce7a57a7f7a2ad128ff8d6bd
LostMoa <life4458@naver.com>
2022-08-21 15:40:43 +0900

    20220821 15:40 업데이트
    마우스 클릭 파티클 추가

---
+++ Assets/Scripts/MouseClickMangaer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseClickMangaer : MonoBehaviour
+{
+    public static MouseClickMangaer instance;
+    GameObject[] particles;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start()
+    {
+        particles = new GameObject[transform.childCount];
+        for (int i=0;i<transform.childCount;i++)
+        {
+            particles[i] = transform.GetChild(i).gameObject;
+        }
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            PlayClickEffect(Input.mousePosition - new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        }
+    }
+
+    void PlayClickEffect(Vector2 _mousePos)
+    {
+        DisableClickEffect();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            particles[i].SetActive(true);
+            particles[i].GetComponent<RectTransform>().position = _mousePos + new Vector2(Screen.width / 2, Screen.height / 2);
+        }
+    }
+
+    void DisableClickEffect()
+    {
+        for (int i = 0; i < particles.Length; i++)
+        {
+            particles[i].SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/MouseClickParticle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClickParticle : MonoBehaviour
+{
+    public float moveSpeed;
+    float particleSize;
+
+    void OnEnable()
+    {
+        GetComponent<RectTransform>().rotation = Quaternion.Euler(0, 0, Random.Range(0, 360));
+        particleSize = Random.Range(0.25f, 1f);
+        gameObject.transform.localScale = new Vector3(particleSize, particleSize, 1);
+        StartCoroutine(DisableParticle());
+        StartCoroutine(MoveParticle());
+    }
+    IEnumerator MoveParticle()
+    {
+        while (true)
+        {
+            transform.Translate(new Vector2(moveSpeed, moveSpeed), Space.Self);
+            yield return new WaitForSecondsRealtime(0.01f);
+        }
+    }
+
+    IEnumerator DisableParticle()
+    {
+        yield return new WaitForSecondsRealtime(Random.Range(0.15f, 0.35f));
+        gameObject.SetActive(false);
+    }
+}
