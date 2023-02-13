01cdde9bd883108e742df23697f21b9c6e514d4d
teet4r <76508241+teet4r@users.noreply.github.com>
2023-02-13 14:07:49 +0900

    230213 Update
    Spade Master가 죽으면 체력 바가 사라지지 않는 버그 수정

--- Assets/Scripts/BattleRound1 Scripts/SpadeMasterHpBar.cs
+++ Assets/Scripts/BattleRound1 Scripts/SpadeMasterHpBar.cs
@@ -26,6 +26,7 @@ public class SpadeMasterHpBar : MonoBehaviour
         }
         else
         {
+            hpBackground.SetActive(false);
             spadeMaster = GameObject.FindGameObjectWithTag("Enemy");
         }
     }
